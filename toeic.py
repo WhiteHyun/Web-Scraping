b28aee93c3325508b192f18d16147e7939ce1bdb
WhiteHyun <whi7ehyun@gmail.com>
2021-10-20 16:37:05 +0900

    .

--- toeic.py
+++ toeic.py
@@ -2,22 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def extract_problem(html):
+    word = html.find("p", {"class": "word"}).string
+    answer = html.find("span", {"class": "af_answer"}).string
+    word = word[word.find(' ') + 1:]
+    return {word: answer}
+
+
 # TOEIC Scraping
 def extract_toeic(day, question):
     URL = f"https://www.hackers.co.kr/?c=s_toeic/new_voca_toeic_testpaper/toeic_study/new_paper&level=1&level_type=1&lang_text=2&question={question}&day1={day}&day2={day}"
     toeic_result = requests.get(URL)
     toeic_soup = BeautifulSoup(toeic_result.text, "html.parser")
-    list_box = toeic_soup.find("ul", {"class": "list-box"})
-    words_list = list_box.find_all("p", {"class": "word"})
-    answers_list = list_box.find_all("span", {"class": "af_answer"})
-
-    word = []
-    answer = []
-    for n in words_list:
-        temp = n.string
-        temp = temp[temp.find(' ') + 1:]
-        word.append(temp)
-
-    for n in answers_list:
-        answer.append(n.string)
-    return (word, answer)
+    list_box = toeic_soup.find("ul", {"class": "list-box"}).find_all("li")
+    toeic_problem = []
+    for problem in list_box:
+        toeic_problem.append(extract_problem(problem))
+    return toeic_problem
